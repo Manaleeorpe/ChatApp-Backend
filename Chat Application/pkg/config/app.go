2857bc1063c9f87cf6c4a73e542afafbf30db516
Manaleeorpe <71582885+Manaleeorpe@users.noreply.github.com>
2025-08-05 17:53:51 +0530

    Update app.go

--- Chat Application/pkg/config/app.go
+++ Chat Application/pkg/config/app.go
@@ -1,41 +1,44 @@
 package config
 
 import (
-    "fmt"
-    "log"
-    "net/url"
-    "os"
-    "strings"
+	"fmt"
+	"log"
+	"net/url"
+	"os"
+	"strings"
 
-    "gorm.io/driver/mysql"
-    "gorm.io/gorm"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var DB *gorm.DB
 
 func Connect() {
-    rawURL := os.Getenv("SQl_URL")
-    if rawURL == "" {
-        log.Fatal("SQl_URL not set")
-    }
+	rawURL := os.Getenv("SQL_URL")
+	if rawURL == "" {
+		log.Fatal("DATABASE_URL not set")
+	}
 
-    u, err := url.Parse(rawURL)
-    if err != nil {
-        log.Fatal("Invalid SQl_URL:", err)
-    }
+	// Parse URL
+	u, err := url.Parse(rawURL)
+	if err != nil {
+		log.Fatal("Invalid DATABASE_URL:", err)
+	}
 
-    user := u.User.Username()
-    pass, _ := u.User.Password()
-    hostPort := u.Host
-    dbname := strings.TrimPrefix(u.Path, "/")
+	user := u.User.Username()
+	pass, _ := u.User.Password()
+	hostPort := u.Host
+	dbname := strings.TrimPrefix(u.Path, "/")
 
-    // Build DSN for MySQL driver
-    dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-        user, pass, hostPort, dbname)
+	// Build DSN for jinzhu gorm
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		user, pass, hostPort, dbname)
 
-    DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
-    if err != nil {
-        log.Fatal("Failed to connect database:", err)
-    }
-    log.Println("Connected to MySQL!")
+	// Open connection
+	DB, err = gorm.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal("Failed to connect database:", err)
+	}
+
+	log.Println("Connected to MySQL!")
 }
