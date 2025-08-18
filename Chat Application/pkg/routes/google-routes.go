e950eaeaf92d920344cff3baaa92941a4fb9e079
Manaleeorpe <71582885+Manaleeorpe@users.noreply.github.com>
2025-08-18 16:37:25 +0530

    Update google-routes.go

--- Chat Application/pkg/routes/google-routes.go
+++ Chat Application/pkg/routes/google-routes.go
@@ -10,5 +10,10 @@ var RegisterGoogleAuthstoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/auth/google/login", auth.HandleGoogleLogin)
 	router.HandleFunc("/auth/google/callback", auth.HandleGoogleCallback)
 	router.HandleFunc("/auth/google/logout", auth.HandleGoogleLogout).Methods("GET")
+	router.HandleFunc("/test",
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("Login successful"))
+		}).Methods("GET")
 
 }
+
