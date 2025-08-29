3e39c995c59792419016c2ae8fabdb3b3bbf1c6d
Manaleeorpe <71582885+Manaleeorpe@users.noreply.github.com>
2025-08-29 13:17:59 +0530

    Update google.go t

--- Chat Application/pkg/auth/google.go
+++ Chat Application/pkg/auth/google.go
@@ -19,7 +19,7 @@ import (
 var Store = sessions.NewCookieStore([]byte("your-secret-key"))
 
 var GoogleOauthConfig = &oauth2.Config{
-	RedirectURL:  "https://nikhilvichare.fyi/auth/google/callback",
+	RedirectURL:  "https://nikhilvichare.fyi/auth/google/callback/",
 	ClientID:     config.GoogleClientID,     //"272806201443-0hot4ej2vc1u8vof49gvmjgvh3m3f01d.apps.googleusercontent.com", //
 	ClientSecret: config.GoogleClientSecret, //.Getenv("GOOGLE_CLIENT_SECRET"), //"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",                                      //
 	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/userinfo.profile"},
@@ -136,6 +136,7 @@ func HandleGoogleCallback(w http.ResponseWriter, r *http.Request) {
 	//redirecting to the react app
 	http.Redirect(w, r, "http://localhost:3000/dashboard", http.StatusSeeOther)
 }
+
 
 
 
